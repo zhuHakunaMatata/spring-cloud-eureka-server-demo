b0f1de1dc99bdc1267eee204fedc24453c14e98f
mrzhu <2692027659@qq.com>
2018-10-09 09:52:13 +0800

    initial

---
+++ src/main/java/com/example/springcloudeurekaserverdemo/SpringCloudEurekaServerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudeurekaserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaServerDemoApplication.class, args);
+	}
+}

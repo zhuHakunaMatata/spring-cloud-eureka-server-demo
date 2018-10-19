6e00a0d746d6f09a7f454e2b6ca277b3fb36c79f
mrzhu <2692027659@qq.com>
2018-10-19 11:17:22 +0800

    --------------

--- src/main/java/com/example/springcloudeurekaserverdemo/SpringCloudEurekaServerDemoApplication.java
+++ src/main/java/com/example/springcloudeurekaserverdemo/SpringCloudEurekaServerDemoApplication.java
@@ -3,6 +3,7 @@ package com.example.springcloudeurekaserverdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableEurekaServer
